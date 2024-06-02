a8e6e608b7f76692ea9c60a0ed646270edd3f4ac
daniel234423 <dnulapi@gmail.com>
2024-06-01 20:10:19 -0400

    se encontro video

---
+++ app/auth/archivos.py
@@ -0,0 +1,4 @@
+import cv2
+
+imagen = cv2.imread()
+video = cv2.VideoCapture()
---
+++ app/auth/main.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from config import Config
+def crear_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    return app
